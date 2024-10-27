72f0419d0b96e4088754973dc8db62d64e09b836
jaemisama <jaemisama5651@gmial.com>
2024-10-28 05:43:44 +0900

    2480

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-A, B = map(int, input().split())
-C = int(input())
+a, b, c = map(int, input().split())
 
+prize = 0
 
-
-B = B + C
-
-if B >= 60:
-    A = A + B // 60
-    B = B % 60
-if A >= 24:
-    A = A % 24
-
-print(A, B)
-    
+if a == b == c:
+    prize = 10000 + a * 1000
+elif a == b or a == c:
+    prize = 1000 + a *100
+elif b == c:
+    prize = 1000 + b *100
+else:
+    prize = max(a,b,c)*100
