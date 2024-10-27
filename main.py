d55eec716235f1639f24fbb541768cc02ecda68c
jaemisama <jaemisama5651@gmial.com>
2024-10-27 13:35:53 +0900

    2884

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-a, b = map(int, input().split())
-print(a / b)
+H, M = map(int, input().split)
+
+
+if M > 45:
+    M -= 45
+else:
+    M = M + 60 - 45
+    H = H - 1 if H > 0 else 23
+
+print(H, M)
